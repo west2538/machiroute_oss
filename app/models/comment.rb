728743f0d552c1ab340a387a52bb71a863c399cf
west2538 <west2538@gmail.com>
2019-05-27 05:54:18 +0900

    v6.0rc6 direct upload

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,15 +4,15 @@ class Comment < ApplicationRecord
   has_many :notifications
   has_one_attached :image
 
-  validates :image, file_size: { in: 5.kilobytes..10.megabytes },
-    file_content_type: { allow: ['image/jpg','image/jpeg', 'image/png', 'image/gif'], message: '写真をアップロードできませんでした' }, on: :create, if: :image_attached?
+  # validates :image, file_size: { in: 5.kilobytes..10.megabytes },
+  #   file_content_type: { allow: ['image/jpg','image/jpeg', 'image/png', 'image/gif'], message: '写真をアップロードできませんでした' }, on: :create, if: :image_attached?
 
   after_commit :commentmstdn_self, on: [:create, :update]
   # after_create_commit { CommentBroadcastJob.perform_later self }
 
-  def image_attached?
-    image.attached?
-  end
+  # def image_attached?
+  #   image.attached?
+  # end
 
   def commentmstdn_self
     if saved_change_to_mstdn_status?
