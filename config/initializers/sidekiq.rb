e45f40bf7c23bca3d0a26a0118c5f8088b4e7cc7
west2538 <west2538@gmail.com>
2019-05-29 10:36:08 +0900

    update REDIS_URL

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
     case Rails.env
       when 'production' then
-        config.redis = { url: 'redis://h:p4a9da922de13785bbc8726216a72635515af95a1f46686858df79a6910c50bfb@ec2-3-213-150-12.compute-1.amazonaws.com:20939', namespace: 'sidekiq' }
+        config.redis = { url: 'redis://h:p4a9da922de13785bbc8726216a72635515af95a1f46686858df79a6910c50bfb@ec2-34-206-59-95.compute-1.amazonaws.com:29469', namespace: 'sidekiq' }
       else
         config.redis = { url: 'redis://127.0.0.1:6379/0', namespace: 'sidekiq' }
     end
