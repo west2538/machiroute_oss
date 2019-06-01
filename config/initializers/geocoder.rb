05d7dcf67e2b836500925acdd1a8906f1dc58ab1
west2538 <west2538@gmail.com>
2019-06-01 10:50:37 +0900

    redis_url dotenv for Heroku

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   use_https: true,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: 'AIzaSyDQ8Wqa56SmA6-ldoKlWSV8z5d6gWP8ux4',               # API key for geocoding service
+  api_key: ENV['GMAP_API_KEY'],               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
@@ -19,10 +19,5 @@ Geocoder.configure(
   # Calculation options
   units: :km,                 # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
-
-# ip_lookup: :google, 
-
-# to use an API key:
-# api_key: "AIzaSyDQ8Wqa56SmA6-ldoKlWSV8z5d6gWP8ux4",
 
 )
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
     case Rails.env
       when 'production' then
-        config.redis = { url: 'redis://h:p4a9da922de13785bbc8726216a72635515af95a1f46686858df79a6910c50bfb@ec2-34-206-59-95.compute-1.amazonaws.com:29469', namespace: 'sidekiq' }
+        config.redis = { url: ENV.fetch('REDIS_URL'), namespace: 'sidekiq' }
       else
         config.redis = { url: 'redis://127.0.0.1:6379/0', namespace: 'sidekiq' }
     end
@@ -10,7 +10,7 @@ end
 Sidekiq.configure_client do |config|
     case Rails.env
         when 'production' then
-        config.redis = { url: 'redis://h:p4a9da922de13785bbc8726216a72635515af95a1f46686858df79a6910c50bfb@ec2-34-206-59-95.compute-1.amazonaws.com:29469', namespace: 'sidekiq' }
+        config.redis = { url: ENV.fetch('REDIS_URL'), namespace: 'sidekiq' }
         else
         config.redis = { url: 'redis://127.0.0.1:6379/0', namespace: 'sidekiq' }
     end
