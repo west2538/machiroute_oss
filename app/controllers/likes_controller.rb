431cef787c64d3de256f5b224dcee1883189e993
west2538 <west2538@gmail.com>
2019-06-01 12:01:22 +0900

    fix calc

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,11 +8,11 @@ class LikesController < ApplicationController
         @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
         @like.save
         flash.now[:notice] = "せーぶ完了！HP+10"
-        @current_user.hp = @current_user.hp + 10
+        @current_user.hp += 10
         if @current_user.hp >= ((@current_user.level * 2) + 68)
             @current_user.hp = ((@current_user.level * 2) + 68)
         end
-        @current_user.machika_token = @current_user.machika_token - 1
+        @current_user.machika_token -= 1
         if @current_user.machika_token < 0
             @current_user.machika_token = 0
             flash.now[:notice] = "せーぶ完了！HP+10💌あなたのMaChiKaが0なので支援できません"
@@ -20,7 +20,7 @@ class LikesController < ApplicationController
             flash.now[:notice] = "せーぶ完了！HP+10💌MaChiKaをサブクエスト投稿者に+1支援"
             @post = Post.find_by(id: params[:post_id])
             @user = User.where(uid: @post.post_uid).order(created_at: :desc).first
-            @user.machika_token = @user.machika_token + 1
+            @user.machika_token += 1
         end
         @current_user.save
         @user.save
@@ -32,12 +32,12 @@ class LikesController < ApplicationController
         @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])
         @like.destroy
         flash.now[:notice] = "せーぶ解除！HP-10"
-        @current_user.hp = @current_user.hp - 10
-        @current_user.machika_token = @current_user.machika_token + 1
+        @current_user.hp -= 10
+        @current_user.machika_token += 1
         flash.now[:notice] = "せーぶ解除！HP-10💌MaChiKaがあなたに+1戻りました"
         @post = Post.find_by(id: params[:post_id])
         @user = User.where(uid: @post.post_uid).order(created_at: :desc).first
-        @user.machika_token = @user.machika_token - 1
+        @user.machika_token -= 1
         if @user.machika_token < 0
             @user.machika_token = 0
         end
