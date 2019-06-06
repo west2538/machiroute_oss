1deca3586144e116b0c1d14b32c5516f7da9dbe0
west2538 <west2538@gmail.com>
2019-06-06 14:45:35 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,15 @@ Rails.application.routes.draw do
   mount GrapeSwaggerRails::Engine => '/docs' if defined? GrapeSwaggerRails
   mount ActionCable.server => '/cable'
 
+  root 'posts#index'
   resources :devices, only: [:create]
+  resources :users
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
+  end
 
   get '.well-known/assetlinks' => "wellknown#assetlinks"
-
-  get 'notifications/link_through'
-  get 'notifications/update_all' => "notifications#update_all"
   get '500', to: 'errors#not_found'
   post "likes/:post_id/create" => "likes#create"
   post "likes/:post_id/destroy" => "likes#destroy"
@@ -21,23 +24,11 @@ Rails.application.routes.draw do
   patch "comments/create" => "comments#create"
   get "posts/auto_complete" => "posts#auto_complete"
 
-  resources :posts do
-    resources :comments, only: [:create, :destroy]
-    resources :likes, only: [:create, :destroy]
-  end
-
-  root 'posts#index'
-
-  resources :users
-
   get "/auth/twitter/callback" => "auth#create"
   delete "/auth/destroy/twitter" => 'auth#destroy', as: :destroy_connection
-
   post "/auth/create/account" => "sessions#create"
   get "/auth/create/account" => "sessions#create"
-
   get "/auth/failure" => "sessions#failure"
-
   get '/auth/:provider/callback', to: 'sessions#create'
   delete "/signout", to: "sessions#destroy"
 
@@ -49,6 +40,8 @@ Rails.application.routes.draw do
   get 'form2' , as: 'form2', to: 'posts#form2'
   get 'form3' , as: 'form3', to: 'posts#form3'
 
+  get 'notifications/link_through'
+  get 'notifications/update_all' => "notifications#update_all"
   get 'notifications/:id/link_through', to: 'notifications#link_through',
   as: :link_through
 
