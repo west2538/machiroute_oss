3c85d9d6d237fcfca209aa7385c85498bf301ec9
west2538 <west2538@gmail.com>
2019-06-13 15:15:16 +0900

    fix validation on Ajax

--- app/assets/javascripts/channels/appearance.js
+++ app/assets/javascripts/channels/appearance.js
@@ -12,6 +12,7 @@ App.appearance = App.cable.subscriptions.create({
       // $('#users-list1 > li').html( function( index, element ) {
         // if ( index === 0 ) {
           // if ( !element.startsWith(user.display_name) ) {
+      if(document.getElementById("users-list1") != null){
       var element = document.getElementById("users-list1");
         element.insertAdjacentHTML("afterbegin", "<li class='list-unstyled font-weight-bold text-success usershadow'>" + user.display_name + " <span class='oi oi-account-login' title='" + user.online_at + "'></span> <small class='text-secondary'><span data-livestamp='" + user.online_at + "'></span></small></li>");
       var element = document.getElementById("users-list2");
@@ -21,15 +22,18 @@ App.appearance = App.cable.subscriptions.create({
           // }
         // }
       // })
+      }
     };
 
     if (user.online === false){
+      if(document.getElementById("users-list1") != null){
       var element = document.getElementById("users-list1");
         element.insertAdjacentHTML("afterbegin", "<li class='list-unstyled font-weight-bold text-danger usershadow'>" + user.display_name + " <span class='oi oi-account-logout' title='" + user.online_at + "'></span> <small class='text-secondary'><span data-livestamp='" + user.online_at + "'></span></small></li>");
       var element = document.getElementById("users-list2");
         element.insertAdjacentHTML("afterbegin", "<li class='list-unstyled font-weight-bold text-danger usershadow'>" + user.display_name + " <span class='oi oi-account-logout' title='" + user.online_at + "'></span> <small class='text-secondary'><span data-livestamp='" + user.online_at + "'></span></small></li>");
       var element = document.getElementById("users-list3");
         element.insertAdjacentHTML("afterbegin", "<li class='list-unstyled font-weight-bold text-danger usershadow'>" + user.display_name + " <span class='oi oi-account-logout' title='" + user.online_at + "'></span> <small class='text-secondary'><span data-livestamp='" + user.online_at + "'></span></small></li>");
+      }
     };
   }
 });
