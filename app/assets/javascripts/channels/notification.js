76c1c29d5cc8a5639c54f0f1e86ec56d30932fb8
west2538 <west2538@gmail.com>
2019-06-29 15:55:58 +0900

    fix btn on notification channel

--- app/assets/javascripts/channels/notification.js
+++ app/assets/javascripts/channels/notification.js
@@ -6,7 +6,7 @@ App.notification = App.cable.subscriptions.create({
     var current_uid = document.getElementsByName('current_user')[0].content;
     if (data.notified_type === 'クリア' && current_uid === data.uid ){
         var target = document.getElementById("dropdownMenu1");
-        target.className = "btn btn-sm btn-success dropdown-toggle";
+        target.className = "btn btn-sm btn-default btn-success dropdown-toggle";
         var element = document.getElementById("mstdn_notification");
         element.insertAdjacentHTML("afterbegin", "<a href='/notifications/" + data.id + "/link_through' class='list-group-item pb-0' style='width:210px;'><div class='small clearfix'><p class='mb-0'><br><span class='font-weight-bold'>あなたのサブクエストが<br><span class='font-weight-bold'>" + data.notified_type + "</span>されました！</p><p class='mt-0' style='float:right;'><span data-livestamp='" + data.created_at + "'></span></p></div></a>");
     };
