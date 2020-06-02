23b3c76b5ac5774b60c4438b6004e6c34b091eae
west2538 <west2538@gmail.com>
2020-06-02 21:23:59 +0900

    Compatible with google-cloud-vision 1.0

--- app/jobs/image_annotate_job.rb
+++ app/jobs/image_annotate_job.rb
@@ -5,8 +5,8 @@ class ImageAnnotateJob < ApplicationJob
  
     tempfile = target.image.attachment.service.send(:object_for, target.image.key).public_url
 
-    require "google/cloud/vision"
-    image_annotator = Google::Cloud::Vision::ImageAnnotator.new
+    require "google/cloud/vision/v1"
+    image_annotator = Google::Cloud::Vision::V1::ImageAnnotator::Client.new
     response = image_annotator.safe_search_detection image: tempfile
     response.responses.each do |res|
     safe_search = res.safe_search_annotation
