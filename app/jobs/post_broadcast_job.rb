2ec70c81ca0c5e3c477643d9d5e023dacccac271
west2538 <west2538@gmail.com>
2020-06-20 22:20:10 +0900

    change asset_path to CloudFront

--- app/jobs/post_broadcast_job.rb
+++ app/jobs/post_broadcast_job.rb
@@ -12,15 +12,17 @@ class PostBroadcastJob < ApplicationJob
       display_name = '@' + cableuser[0]
     end
     unless post.bookcover.present?
-      post.bookcover = '/assets/nobookcover.jpg'
+      post.bookcover = asset_path('nobookcover.jpg')
     end
     unless post.newsimage.present?
-      post.newsimage = '/assets/ogpimage.png'
+      post.newsimage = asset_path('ogpimage.png')
     end
     cablebokensha = '/users/' + @user.id.to_s
     cabledomain = cableuser[1]
     cabledate1 = post.updated_at.strftime('%-m月%-d日 %-H:%M')
     cabledate2 = post.updated_at.strftime('%Y-%m-%d %H:%M:%S')
+    missing = asset_path('missing.png')
+    ogpmiage = asset_path('ogpimage.png')
     if post.title == 'ニュース'
       if post.newstitle.present?
         newstitle = post.newstitle.truncate(46)
@@ -30,7 +32,7 @@ class PostBroadcastJob < ApplicationJob
     end
     URI.extract(post.body).uniq.each {|url| post.body.gsub!(url, '')}
     
-    ActionCable.server.broadcast 'post_channel', {body: post.body, id: post.id, title: post.title, uid: post.post_uid, avatar: @user.avatar, level: @user.level, cableuser: cableuser, cabledomain: cabledomain, cablebokensha: cablebokensha, display_name: display_name, cabledate1: cabledate1, cabledate2: cabledate2, booktitle:post.booktitle, bookpublisher:post.bookpublisher, bookauthor:post.bookauthor, bookcover:post.bookcover, newsurl:post.newsurl, newsimage:post.newsimage, newstitle:newstitle, stationname:post.stationname, mstdn_status:post.mstdn_status}
+    ActionCable.server.broadcast 'post_channel', {body: post.body, id: post.id, title: post.title, uid: post.post_uid, avatar: @user.avatar, level: @user.level, cableuser: cableuser, cabledomain: cabledomain, cablebokensha: cablebokensha, display_name: display_name, cabledate1: cabledate1, cabledate2: cabledate2, missing: missing, ogpimage: ogpimage, booktitle:post.booktitle, bookpublisher:post.bookpublisher, bookauthor:post.bookauthor, bookcover:post.bookcover, newsurl:post.newsurl, newsimage:post.newsimage, newstitle:newstitle, stationname:post.stationname, mstdn_status:post.mstdn_status}
 
   end
 
